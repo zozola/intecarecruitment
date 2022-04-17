4ec1b7cb6e05c1a081f346052e9b024683d549a3
Aleksandra Zagrabska <ozagrabska@gmail.com>
2022-04-17 14:18:59 +0200

    FamilyApp template generated using spring.initializr
    Aplication supports single empty mapping "CreateFamily".

---
+++ README.md
@@ -0,0 +1,12 @@
+# Evaluation excercise
+
+Source code for evaluation excercise for the recruitment process for Inteca company.
+
+## familyapp
+
+Running familyapp:
+
+```
+$ cd ozagrabska.familyapp
+$ ./mvnw spring-boot:run
+```
---
+++ ozagrabska.familyapp/src/main/java/com/inteca/ozagrabska/familyapp/Application.java
@@ -0,0 +1,23 @@
+package com.inteca.ozagrabska.familyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/CreateFamily")
+	public String CreateFamily() {
+		return "Not supported";
+	}
+
+
+}
